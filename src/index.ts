b5c3027d9bacc703d70eba37b847d637177447c6
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-07-03 16:32:43 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import "reflect-metadata";
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+// import { Post } from "./entities/Post";
+import microConfig from "./mikro-orm.config"
+import express from "express";
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { PostResolver } from "./resolvers/post";
+import { UserResolver } from "./resolvers/user";
+
+const main = async () => {
+   const orm = await MikroORM.init(microConfig);
+   await orm.getMigrator().up();
+
+   const app = express();
+
+   const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+         resolvers: [PostResolver, UserResolver],
+         validate: false,
+      }),
+      context: () => ({em: orm.em})
+   });
+
+   apolloServer.applyMiddleware({ app });
+
+   app.listen(4000, () => {
+      console.log("server started on localhost:4000")
+   })
+
+
+   // const post = orm.em.create(Post, {title: 'my first post'});
+   // await orm.em.persistAndFlush(post);
+   // const posts = await orm.em.find(Post, {});
+   // console.log(posts)
+};
+
+main();
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,19 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core"
+import path from 'path'
+import { User } from "./entities/User";
+
+export default {
+   migrations: {
+      path: path.join(__dirname, "./migrations"),
+      pattern: /^[\w-]+\d+\.[tj]s$/,
+   },
+   entities: [Post, User],
+   dbName: 'lireddit',
+   user: 'postgres',
+   password: 'postgres',
+   type: 'postgresql',
+   debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
+/* stick a type in the brackets by typeof (init is a function), it returns an array so we grab a first parameter */ 
