4617a2df74320eedc54e6abdfdf00996523841f5
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-08-05 17:29:01 +0200

    added cors policy

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,49 @@
-import "reflect-metadata";
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import 'reflect-metadata'
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants'
 // import { Post } from "./entities/Post";
-import microConfig from "./mikro-orm.config"
-import express from "express";
-import { ApolloServer } from 'apollo-server-express';
-import { buildSchema } from 'type-graphql';
-import { PostResolver } from "./resolvers/post";
-import { UserResolver } from "./resolvers/user";
-import redis from 'redis';
-import session from 'express-session';
+import microConfig from './mikro-orm.config'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { buildSchema } from 'type-graphql'
+import { PostResolver } from './resolvers/post'
+import { UserResolver } from './resolvers/user'
+import redis from 'redis'
+import session from 'express-session'
 import connectRedis from 'connect-redis'
+import cors from 'cors'
 
 const main = async () => {
-   const orm = await MikroORM.init(microConfig);
-   await orm.getMigrator().up();
+   const orm = await MikroORM.init(microConfig)
+   await orm.getMigrator().up()
 
-   const app = express();
+   const app = express()
 
    const RedisStore = connectRedis(session)
    const redisClient = redis.createClient()
 
    app.use(
+      cors({
+         origin: 'http://localhost:3000',
+         credentials: true,
+      })
+   )
+   app.use(
       session({
          name: 'qid',
-         store: new RedisStore({ 
+         store: new RedisStore({
             client: redisClient,
-            disableTouch: true
+            disableTouch: true,
          }),
          cookie: {
             maxAge: 1000 * 60 * 60 * 24 * 365 * 10, // 10 years
             httpOnly: true,
             sameSite: 'lax', // csrf
-            secure: __prod__ // cookie only works in https
+            secure: __prod__, // cookie only works in https
          },
          saveUninitialized: false,
-         secret: "super secret variable",
-         resave: false
+         secret: 'super secret variable',
+         resave: false,
       })
    )
 
@@ -45,20 +52,17 @@ const main = async () => {
          resolvers: [PostResolver, UserResolver],
          validate: false,
       }),
-      context: ({ req, res }) => ({em: orm.em, req, res }),
-   });
-
-   apolloServer.applyMiddleware({ app });
-
-   app.listen(4000, () => {
-      console.log("server started on localhost:4000")
+      context: ({ req, res }) => ({ em: orm.em, req, res }),
    })
 
+   apolloServer.applyMiddleware({
+      app,
+      cors: false,
+   })
 
-   // const post = orm.em.create(Post, {title: 'my first post'});
-   // await orm.em.persistAndFlush(post);
-   // const posts = await orm.em.find(Post, {});
-   // console.log(posts)
-};
+   app.listen(4000, () => {
+      console.log('server started on localhost:4000')
+   })
+}
 
-main();
+main()
