4ef028d4415675d66ccd1591dbe6608887eb7de6
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-08-09 23:28:27 +0200

    change password implementation

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -147,7 +147,10 @@ export class UserResolver {
    }
 
    @Mutation(() => Boolean)
-   async forgotPassword(@Arg('email') email: string, @Ctx() { em, redis }: MyContext) {
+   async forgotPassword(
+      @Arg('email') email: string,
+      @Ctx() { em, redis }: MyContext
+   ) {
       const user = await em.findOne(User, { email })
       if (!user) {
          // the email is not in db
@@ -155,11 +158,65 @@ export class UserResolver {
       }
 
       const token = v4()
-      await redis.set(FORGET_PASSWORD_PREFIX + token, user.id, 'ex', 1000 * 60 * 60 * 24) // a day
+      await redis.set(
+         FORGET_PASSWORD_PREFIX + token,
+         user.id,
+         'ex',
+         1000 * 60 * 60 * 24
+      ) // a day
       await sendEmail(
          email,
          `<a href="http://localhost:3000/change-password/${token}">reset password</a>`
       )
       return true
+   }
+
+   @Mutation(() => UserResponse)
+   async changePassword(
+      @Arg('token') token: string,
+      @Arg('newPassword') newPassword: string,
+      @Ctx() { em, redis, req }: MyContext
+   ): Promise<UserResponse> {
+      // TODO: extract this logic from validateRegister
+      if (newPassword.length <= 5) {
+         return {
+            errors: [
+               {
+                  field: 'newPassword',
+                  message: 'length must be greater than 5',
+               },
+            ],
+         }
+      }
+      const userId = await redis.get(FORGET_PASSWORD_PREFIX + token)
+      if (!userId) {
+         return {
+            errors: [
+               {
+                  field: 'token',
+                  message: 'token expired',
+               },
+            ],
+         }
+      }
+      const user = await em.findOne(User, { id: parseInt(userId) })
+
+      if (!user) {
+         return {
+            errors: [
+               {
+                  field: 'token',
+                  message: 'user no longer exists',
+               },
+            ],
+         }
+      }
+      user.password = await argon2.hash(newPassword)
+      await em.persistAndFlush(user)
+
+      // log in user after change password
+      req.session.userId = user.id
+
+      return { user }
    }
 }
