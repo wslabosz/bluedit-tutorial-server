2dcebd23558b50f424f628b4871a2f75a993d491
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-08-27 17:12:57 +0200

    added flag for Pagination Post resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -7,6 +7,7 @@ import {
    InputType,
    Int,
    Mutation,
+   ObjectType,
    Query,
    Resolver,
    Root,
@@ -24,30 +25,43 @@ class PostInput {
    text: string
 }
 
+@ObjectType()
+class PaginatedPosts {
+   @Field(() => [Post])
+   posts: Post[]
+   @Field()
+   hasMore: boolean
+}
+
 @Resolver(Post)
 export class PostResolver {
    @FieldResolver(() => String)
-   textSnippet(@Root() root: Post) {
-      return root.text.slice(0, 50)
+   textSnippet(@Root() post: Post) {
+      return post.text.slice(0, 50)
    }
 
-   @Query(() => [Post])
+   @Query(() => PaginatedPosts)
    async posts(
       @Arg('limit', () => Int) limit: number,
       @Arg('cursor', () => String, { nullable: true }) cursor: string | null
-   ): Promise<Post[]> {
+   ): Promise<PaginatedPosts> {
       const realLimit = Math.min(50, limit)
+      const realLimitPlusOne = realLimit + 1
       const qb = getConnection()
          .getRepository(Post)
          .createQueryBuilder('p')
          .orderBy('"createdAt"', 'DESC')
-         .take(realLimit)
       if (cursor) {
-         qb.where('"createdAt < :cursor"', {
+         qb.where('"createdAt" < :cursor', {
             cursor: new Date(parseInt(cursor)),
          })
       }
-      return qb.getMany()
+
+      const posts = await qb.limit(realLimitPlusOne).getMany()
+      return {
+         posts: posts.slice(0, realLimit),
+         hasMore: posts.length === realLimitPlusOne,
+      }
    }
 
    @Query(() => Post, { nullable: true })
