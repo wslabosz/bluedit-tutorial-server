f2191e70aa651555aad2f6c36ec558f07176e2a0
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-08-29 01:56:05 +0200

    added creator fetching

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -47,17 +47,31 @@ export class PostResolver {
    ): Promise<PaginatedPosts> {
       const realLimit = Math.min(50, limit)
       const realLimitPlusOne = realLimit + 1
-      const qb = getConnection()
-         .getRepository(Post)
-         .createQueryBuilder('p')
-         .orderBy('"createdAt"', 'DESC')
+
+      const replacements: any[] = [realLimitPlusOne]
       if (cursor) {
-         qb.where('"createdAt" < :cursor', {
-            cursor: new Date(parseInt(cursor)),
-         })
+         replacements.push(new Date(parseInt(cursor)))
       }
 
-      const posts = await qb.limit(realLimitPlusOne).getMany()
+      const posts = await getConnection().query(
+         `
+         select p.*,
+         json_build_object(
+            'id', u.id,
+            'username', u.username,
+            'email', u.email,
+            'createdAt', u."createdAt"
+            ) "createdBy"
+         from post p
+         inner join public.user u on u.id = p."creatorId"
+         ${cursor ? `where p."createdAt" < $2` : ''}
+         order by p."createdAt" DESC
+         limit $1
+         `,
+         replacements
+      )
+      console.log(posts)
+
       return {
          posts: posts.slice(0, realLimit),
          hasMore: posts.length === realLimitPlusOne,
