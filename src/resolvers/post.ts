3bcadff2df2526241b10a44cdd2a26dcf571c20c
Wojciech Slabosz <230000@edu.p.lodz.pl>
2021-10-08 23:52:10 +0200

    upvote errors handled

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,4 +1,5 @@
 import { Post } from '../entities/Post'
+import { Upvote } from '../entities/Upvote'
 import {
    Arg,
    Ctx,
@@ -115,25 +116,58 @@ export class PostResolver {
    }
 
    @Mutation(() => Boolean)
+   @UseMiddleware(isAuth)
    async vote(
       @Arg('postId', () => Int) postId: number,
-      @Arg('upvoteValue', () => Int) value: number,
+      @Arg('value', () => Int) value: number,
       @Ctx() { req }: MyContext
    ) {
-      const { userId } = req.session
       const isUpvote = value !== -1
       const realValue = isUpvote ? 1 : -1
-      await getConnection().query(
-         `
-         START TRANSACTION;
-         insert into upvote ("userId", "postId", value)
-         values(${userId}, ${postId}, ${realValue});
-         update post 
-         set points = points + ${realValue}
-         where id = ${postId};
-         COMMIT;
-         `
-      )
-      return true
+      const { userId } = req.session
+      const upvote = await Upvote.findOne({ where: { userId: userId, postId: postId} })
+      console.log(upvote)
+
+      // user has voted on the post already
+      // and changing their vote
+      if (upvote && upvote.value !== realValue) {
+         await getConnection().transaction(async (tm) => {
+            await tm.query(
+               `
+               update upvote
+               set value = $1
+               where "postId" = $2 and "userId" = $3
+            `,
+               [realValue, postId, userId]
+            )
+            await tm.query(
+               `
+               update post 
+               set points = points + $1
+               where id = $2
+            `,
+               [2 * realValue, postId]
+            )
+         })
+      } else if (!upvote) {
+         // has never voted before
+         await getConnection().transaction(async (tm) => {
+            await tm.query(
+               `insert into upvote ("userId", "postId", value)
+            values($1, $2, $3);
+            `,
+               [userId, postId, realValue]
+            );
+
+            await tm.query(
+               `update post 
+            set points = points + $1
+            where id = $2
+            `,
+               [realValue, postId]
+            )
+         })
+      }
+      return true;
    }
 }
